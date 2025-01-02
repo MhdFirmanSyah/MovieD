5145419e41bd50ab388c26fd767f3eef6c710dcd
Muhammad Firmansyah <mhdfirmansyah748@gmail.com>
2025-01-02 11:54:49 +0700

    update

---
+++ src/components/Popular Section/Card.jsx
@@ -0,0 +1,18 @@
+const Card = () => {
+  return (
+    <div className="z-50   border text-center rounded-md relative ">
+      <img
+        className=" h-[11rem] z-50  bg-black rounded-t-md"
+        src="./src/assets/imgdevstage/poster.jpg"
+        alt=""
+      />
+      {/* <div className="absolute bottom-0 p-1 rounded-b-md backdrop-blur-sm m-2 left-0 right-0 bg-white/50 h-20"> */}
+      <h1 className="text-wrap max-w-[5px] w-3 sm:max-w-[50%] md:max-w-[45%] ">
+        titfffffffffffffffffffdddddddddddddfffffffffffffle
+      </h1>
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Popular Section/PopularSection.jsx
+++ src/components/Popular Section/PopularSection.jsx
@@ -1,10 +1,17 @@
+import Card from "./Card";
+
 const PopularSection = () => {
+  const arr = [1, 6, 4, 10, 20, 7, 2, 5, 3, 8];
+  console.log(arr);
   return (
-    <div className="container color-white x-10 flex bg- m-auto">
-      <h1>Popular</h1>
+    <div className=" color-white x-10 flex bg-red-400 m-auto justify-center">
+      <div className="container border-black border p-5  mx-5 flex flex-wrap justify-center gap-4 ">
+        {arr.map((arr) => (
+          <Card key={arr} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default PopularSection;
- 
