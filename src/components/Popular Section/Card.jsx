bcffd1c33d31d2654cc6be560c5dccaf0e011ff5
Muhammad Firmansyah <mhdfirmansyah748@gmail.com>
2025-01-16 22:55:09 +0700

    up

--- src/components/Popular Section/Card.jsx
+++ src/components/Popular Section/Card.jsx
@@ -1,17 +1,17 @@
 const Card = () => {
   return (
-    <div className=" border  rounded-xl shadow-md shadow-white dark:shadow-black relative">
-      <img
-        className=" h-[43.5vw] sm:h-[40vw] md:h-[26vw] lg:h-[20vw] z-50  bg-black rounded-t-xl"
-        src="./src/assets/imgdevstage/poster.jpg"
-        alt=""
-      />
-      {/* <div className="flex"> */}
-      <p className=" text-sm text-white text-center p-4 text-wrap">
-        venom the last dance
-      </p>
-      {/* </div> */}
-    </div>
+    <a className="border rounded-xl shadow-md shadow-white dark:shadow-black flex flex-col md:max-w-[17vw] lg:max-w-[13vw] ">
+      <div className="w-full lg:max-w-[13vw]">
+        <img
+          className="h-[42.5vw] sm:h-[39vw] md:h-[25vw] md:w-[17vw] lg:h-[19vw] lg:w-[13vw] z-50 bg-black rounded-t-xl "
+          src="./src/assets/imgdevstage/poster.jpg"
+          alt=""
+        />
+      </div>
+      <div className="flexbox-title text-wrap text-center dark:text-white max-w-[25vw] text-[.7rem]">
+        venom the last dancsdsdsssssssssssssssssss
+      </div>
+    </a>
   );
 };
 
